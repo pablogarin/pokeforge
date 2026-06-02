0fc2381a9cf4e877c673c6adbbf27eb91bb2ab44
pablogarin <pablo.garin@hotmail.com>
2026-06-02 14:25:12 -0500

    refactored code to remove database logic and use the repostiroy method

--- pokeforge-services/service-iv-calc/graphql_engine/resolvers.py
+++ pokeforge-services/service-iv-calc/graphql_engine/resolvers.py
@@ -4,24 +4,19 @@ from database import PokemonRepository
 
 
 def resolve_global_pokedex() -> List[GlobalPokemon]:
-    try:
-        rows = PokemonRepository.fetch_all_global_pokemons()
+    rows = PokemonRepository.fetch_all_global_pokemons()
 
-        return [
-            GlobalPokemon(
-                id=row["id"],
-                name=row["name"].capitalize(),
-                types=row["types"],
-                base_hp=row["base_hp"],
-                base_attack=row["base_attack"],
-                base_defense=row["base_defense"],
-                base_sp_attack=row["base_sp_attack"],
-                base_sp_defense=row["base_sp_defense"],
-                base_speed=row["base_speed"],
-            )
-            for row in rows
-        ]
-
-    except Exception as err:
-        print(f"Error getting pokemons from database: {err}")
-        return []
+    return [
+        GlobalPokemon(
+            id=row["id"],
+            name=row["name"].capitalize(),
+            types=row["types"],
+            base_hp=row["base_hp"],
+            base_attack=row["base_attack"],
+            base_defense=row["base_defense"],
+            base_sp_attack=row["base_sp_attack"],
+            base_sp_defense=row["base_sp_defense"],
+            base_speed=row["base_speed"],
+        )
+        for row in rows
+    ]
